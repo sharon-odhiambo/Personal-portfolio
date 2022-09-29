afaef77e3ed883290c213248b5cb02957a2d1bfb
sharon-odhiambo <88780311+sharon-odhiambo@users.noreply.github.com>
2022-09-29 23:18:21 +0300

    update personal projects

---
+++ src/images/images .js
@@ -0,0 +1,26 @@
+const salama = require('./salama-conference.PNG');
+const world = require('./world-countries.PNG');
+const space = require('./space-x.PNG');
+const lead = require('./leaderboard.png');
+const list = require('./to-do.png');
+const vet = require('./vet-database.PNG');
+const gh = require('./gh.png');
+const twit = require('./twitter.gif');
+const angel = require('./angelist.png');
+const node = require('./node.png');
+const link = require('./link.gif');
+const git = require('./github.gif');
+const html = require('./html.png');
+const css = require('./css.png');
+const jav = require('./js.png');
+const ts = require('./ts.png');
+const react = require('./react.png');
+const red = require('./redux.png');
+const sq = require('./sql.png');
+const rb = require('./ruby.png');
+const res = require('./resume.pdf');
+
+export {
+  salama, world, space, lead, list, vet, gh,
+  twit, angel, node, git, link, html, css, jav, ts, react, red, sq, rb, res,
+};
